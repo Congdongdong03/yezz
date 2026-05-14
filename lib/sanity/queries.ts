b2c51eec77797382f7c9e77ddfee985f197bac8f
Wesley <congdongdong03@gmail.com>
2026-05-14 13:53:12 +1000

    feat(queries): add homepage featured queries

--- lib/sanity/queries.ts
+++ lib/sanity/queries.ts
@@ -40,3 +40,42 @@ export const galleryQuery = `*[_type == "galleryImage"] | order(order asc) {
 }`;
 
 export const siteSettingsQuery = `*[_type == "siteSettings"][0]`;
+
+export const featuredProjectsQuery = `*[_type == "diyProject"] | order(order asc) [0...4] {
+  _id,
+  name,
+  slug,
+  "category": category->name,
+  description,
+  "imageUrl": images[0].asset->url,
+  priceRange,
+  duration,
+  tags,
+  order
+}`;
+
+export const featuredPartiesQuery = `*[_type == "partyPackage"] | order(_createdAt asc) [0...2] {
+  _id,
+  name,
+  slug,
+  description,
+  "imageUrl": images[0].asset->url,
+  minPeople,
+  maxPeople,
+  priceIndicator,
+  tags
+}`;
+
+export const galleryHighlightQuery = `*[_type == "galleryImage"] | order(order asc) [0...6] {
+  _id,
+  "imageUrl": image.asset->url,
+  category,
+  caption,
+  order
+}`;
+
+export const storeVibesQuery = `*[_type == "galleryImage" && category == "store"] | order(order asc) [0] {
+  _id,
+  "imageUrl": image.asset->url,
+  caption
+}`;
